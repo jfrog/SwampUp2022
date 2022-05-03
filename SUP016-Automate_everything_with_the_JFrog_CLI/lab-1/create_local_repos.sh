8d3658d8e1d5db43d87ac154051efe75728d9012
MaharshiPatel <maharshi11patel@gmail.com>
2022-05-03 11:07:51 -0700

    updated lab1 and added rescue script

--- SUP016-Automate_everything_with_the_JFrog_CLI/lab-1/create_local_repos.sh
+++ SUP016-Automate_everything_with_the_JFrog_CLI/lab-1/create_local_repos.sh
@@ -1,4 +1,4 @@
-for row in $(cat ./repos.json | jq -r '.[] | @base64'); do
+for row in $(cat ./local-repos.json | jq -r '.[] | @base64'); do
     _jq() {
       echo ${row} | base64 --decode | jq -r ${1}
     }
