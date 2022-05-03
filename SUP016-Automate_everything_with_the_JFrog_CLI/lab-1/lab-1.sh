d2764f4b071df5de0ddd22370cdaec53cdc40eae
MaharshiPatel <maharshi11patel@gmail.com>
2022-05-03 09:55:55 -0700

    pushed lab1 rescue script

--- SUP016-Automate_everything_with_the_JFrog_CLI/lab-1/lab-1.sh
+++ SUP016-Automate_everything_with_the_JFrog_CLI/lab-1/lab-1.sh
@@ -3,10 +3,6 @@ for row in $(cat ./repos.json | jq -r '.[] | @base64'); do
       echo ${row} | base64 --decode | jq -r ${1}
     }
     #echo "key:"$(_jq '.key')
-    key=$(_jq '.key')
-    packageType=$(_jq '.packageType')
-    rclass=$(_jq '.rclass')
-    repoLayoutRef=$(_jq '.repoLayoutRef')
-    xrayIndex=$(_jq '.xrayIndex')
-    jf rt repo-create template-local.json --vars "repo-name=$key;package-type=$packageType;rclass=$rclass;repoLayoutRef=$repoLayoutRef"
+
+    jf rt repo-create template-local.json --vars "repo-name=$(_jq '.key');package-type=$(_jq '.packageType');repo-type=$(_jq '.rclass');repo-layout=$(_jq '.repoLayoutRef');xray-enable=$(_jq '.xrayIndex')"
 done
