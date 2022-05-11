c68e0d2f0fd421debaee3ffe57d5f74686ce5b47
MaharshiPatel <maharshi11patel@gmail.com>
2022-05-11 16:55:45 -0700

    added rescue script

--- SUP016-Automate_everything_with_the_JFrog_CLI/back/src/increment.sh
+++
@@ -1,4 +0,0 @@
-if sed --version
-then sed -r 's/(currentVersion)=([0-9]+).([0-9]+).([0-9]+)/echo "\1=\2.\3.$BUILD_NUMBER"/e' gradle.properties -i
-else gsed -r 's/(currentVersion)=([0-9]+).([0-9]+).([0-9]+)/echo "\1=\2.\3.$BUILD_NUMBER"/e' gradle.properties -i
-fi
