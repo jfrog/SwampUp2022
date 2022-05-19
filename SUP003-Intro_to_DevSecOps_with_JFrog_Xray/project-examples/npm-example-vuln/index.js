cb71619530ce0f6070302e1875737846cbab703a
Asim Parekh <asimp@asimp-mac-95480.local>
2022-05-19 16:50:34 -0700

    npm builds changed to use much less number of deps

--- SUP003-Intro_to_DevSecOps_with_JFrog_Xray/project-examples/npm-example-vuln/index.js
+++ SUP003-Intro_to_DevSecOps_with_JFrog_Xray/project-examples/npm-example-vuln/index.js
@@ -1,53 +1,69 @@
 const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const _ = require("lodash")
-const mathjs =require("mathjs")
+const app = express();
 
-const app = express()
-const port = 3000
+const serverPort = 80;
 
-app.use(morgan('tiny'))
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send(`🐸 Welcome to SWAMPUP-2021 🐸`)
-})
+// start server
+app.listen(serverPort, () => {
+    console.log("Server running on port " + serverPort);
 
-app.get('/session', (req, res) => {
-    res.send(`🐸 SU-201 Intro to DevSecOps with JFrog Xray 🐸`)
-})
-
-let nums = [1, 2, 3, 4, 5, 6, 7, 8];
- 
-console.log(_.nth(nums, 3));
-console.log(_.nth(nums, -3));
-
-console.log(mathjs.random(1,1000000000000));
-
-app.use((req, res, next) => {
-    const error = new Error("Not found");
-    error.status = 404;
-    error.message = "🐸 Ribbit Ribbit Ribbit Ribbit 🐸"
-    next(error);
-});
-
-// error handler middleware
-app.use((error, req, res, next) => {
-    res.status(error.status || 500).send({
-      error: {
-        status: error.status || 500,
-        message: error.message || 'Internal Server Error',
-      },
+    // check authentication endpoint
+    app.get("/api/checkauth", (req, res, next) => {
+        res.json({"valid" : true,"error" : ""});
     });
-  });
 
-const HelloFrogs = {
-    message: "🐸 Welcome to SWAMPUP-2021 🐸"
-}
+    // check component endpoint
+    app.post("/api/checkcomponent", (req, res, next) => {
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+        let isSafe = true;
+        let currentComponentId = "";
 
-module.exports.HelloFrogs = HelloFrogs;
+        if(req.body.components){
+            for (let i = 0; i < req.body.components.length; i++) {
+                currentComponentId = req.body.components[i].component_id;
+                if(currentComponentId && currentComponentId.includes("swampup-devsecops")) {
+                    console.log("vulnerability found: " + currentComponentId);
+                    isSafe = false;
+                    break;
+                } else {
+                    console.log("safe dependency found: " + currentComponentId);
+                }
+            }
+        }
+
+        if(isSafe){
+            res.json({"components" : []});
+        } else {
+            res.json({
+                "components" : [
+                    {
+                        "component_id" : "" + currentComponentId,
+                        "licenses" : ["Apache 2.0"],
+                        "provider" : "Internal Security Provider",
+                        "vulnerabilities" : [
+                            {
+                                "cve" : "CVE-4242-4242",
+                                "type" : "security",
+                                "source_id" : "42424242",
+                                "summary" : "This component is blacklisted internally",
+                                "description" : "Decision was made to get rid of packages using Struts dependency",
+                                "cvss_v2" : "8.5",
+                                "url" : "http://foo.bar",
+                                "publish_date" : "2021-21-01T20:30:00.000+00:00",
+                                "references" : [    "http://my.internal.wiki.com/" + currentComponentId + ".html"
+                                ]
+                            }
+                        ]}
+                ]
+            });
+        }
+    });
+
+    // Receive webhook endpoint
+    app.post("/api/webhook", (req, res, next) => {
+        console.log(req.body);
+        res.status(200);
+    });
+});
