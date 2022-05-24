2965284ba1faa8a8b020c92cef7a6a15189039aa
gowthamn <chanti529@yahoo.com>
2022-05-24 14:25:06 -0700

    fixed .env file

--- SUP009-DevOps_Automation_with_JFrog_Artifactory_Advanced/lab4-repository_and_project_provisioning/sharing-repositories.sh
+++ SUP009-DevOps_Automation_with_JFrog_Artifactory_Advanced/lab4-repository_and_project_provisioning/sharing-repositories.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-yq r $SCRIPT_DIR/lab-1/repo-conf-creation-main.yaml -j | jq -r '.[]|keys[]' |
+yq r $SCRIPT_DIR/xxx/repo-conf-creation-main.yaml -j | jq -r '.[]|keys[]' |
 while read -r repo_id; do
     curl -X PUT https://$JFROG_PLATFORM/access/api/v1/projects/_/share/repositories/$repo_id/su115 -H "accept: application/json" -H "Authorization: Bearer ${token}"
 done
