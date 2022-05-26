080a13b308ead0019adba4885f242c2edb54a79b
Suresh Venkatesan <suresh.k.venkatesan@gmail.com>
2022-05-25 19:48:20 -0700

    Update sharing-repositories.sh

--- SUP009-DevOps_Automation_with_JFrog_Artifactory_Advanced/lab4-repository_and_project_provisioning/sharing-repositories.sh
+++ SUP009-DevOps_Automation_with_JFrog_Artifactory_Advanced/lab4-repository_and_project_provisioning/sharing-repositories.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-yq r $SCRIPT_DIR/lab4-repository_and_project_provisioning/repo-conf-creation-main.yaml -j | jq -r '.[]|keys[]' |
+yq e $SCRIPT_DIR/lab4-repository_and_project_provisioning/repo-conf-creation-main.yaml -o=j -I=0 |  jq -r '.[]|keys[]' |
 while read -r repo_id; do
-    curl -X PUT https://$JFROG_PLATFORM/access/api/v1/projects/_/share/repositories/$repo_id/su115 -H "accept: application/json" -H "Authorization: Bearer ${token}"
+    curl -X PUT https://$ARTIFACTORY_HOSTNAME/access/api/v1/projects/_/share/repositories/$repo_id/su115 -H "accept: application/json" -H "Authorization: Bearer ${ARTIFACTORY_ACCESS_TOKEN}"
 done
